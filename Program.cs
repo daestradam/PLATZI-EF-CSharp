e908ff917349a1c0a28dffa3b65ad1dcd9f5c2d4
Daniel <daestradam@unal.edu.co>
2024-07-04 23:11:43 -0500

    Conexion a SQLServer. appsettings

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ using proyectoEF;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+// builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
 
 var app = builder.Build();
 
