9956b94e0314a070cffdabdf15b6731838229edc
Daniel <daestradam@unal.edu.co>
2024-07-15 17:28:51 -0500

    Modelos con Fluent API para reemplazar Data Notations

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -3,20 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace proyectoEF.Models;
 
-[Table("Tarea")] //El nombre que uno quiera que quede en la DB
+//[Table("Tarea")] //El nombre que uno quiera que quede en la DB
 public class Tarea {
-    [Key]
+    //[Key]
     public Guid TareaId {get; set;}
-    [ForeignKey("CategoriaId")]
+    //[ForeignKey("CategoriaId")]
     public Guid CategoriaId {get; set;}
-    [Required]
-    [MaxLength(200, ErrorMessage="Titulo debe tener 200 carácteres o más"), MinLength(50)]
+    //[Required]
+    //[MaxLength(200, ErrorMessage="Titulo debe tener 200 carácteres o más"), MinLength(50)]
     public string Titulo {get; set;}
     public string Descripcion {get; set;}
     public Prioridad PrioridadTarea {get; set;}
     public DateTime FechaCreacion {get; set;}
     public virtual Categoria Categoria {get; set;}
-    [NotMapped]
+    //[NotMapped]
     public string Resumen {get;set;}
 }
 public enum Prioridad {
