99f1cdaa3852ff0fc069580261e140f3e60ae7e2
Daniel <daestradam@unal.edu.co>
2024-07-17 17:58:12 -0500

    Actualizar datos

--- Program.cs
+++ Program.cs
@@ -36,6 +36,23 @@ app.MapPost("/api/tareas", async([FromServices] TareasContext dbContext, [FromBo
     return Results.Ok();
 });
 
+app.MapPut("/api/tareas/{id}", async([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) => 
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+    if(tareaActual!=null){
+        tareaActual.CategoriaId=tarea.CategoriaId;
+        tareaActual.Titulo=tarea.Titulo;
+        tareaActual.PrioridadTarea=tarea.PrioridadTarea;
+        tareaActual.Descripcion=tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+    
+    return Results.NotFound();
+});
+
 app.MapGet("/api/categorias", async([FromServices] TareasContext dbContext) => 
 {
     return Results.Ok(dbContext.Categorias.Where(p => p.Peso==20));
