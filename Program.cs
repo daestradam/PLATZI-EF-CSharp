53602deea2cced45224e0832885cc234247dfefc
Daniel <daestradam@unal.edu.co>
2024-07-17 18:09:43 -0500

    Eliminar datos

--- Program.cs
+++ Program.cs
@@ -53,6 +53,20 @@ app.MapPut("/api/tareas/{id}", async([FromServices] TareasContext dbContext, [Fr
     return Results.NotFound();
 });
 
+app.MapDelete("/api/tareas/{id}", async([FromServices] TareasContext dbContext, [FromRoute] Guid id) => 
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+    if(tareaActual!=null){
+        dbContext.Remove(tareaActual);
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+    
+    return Results.NotFound();
+});
+
 app.MapGet("/api/categorias", async([FromServices] TareasContext dbContext) => 
 {
     return Results.Ok(dbContext.Categorias.Where(p => p.Peso==20));
